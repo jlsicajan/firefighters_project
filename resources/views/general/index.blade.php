b5dfe599ea1772e74e99f25b6d58d21dbd2cac17
Jose Luis Sicajan Coy <sicajancoy@gmail.com>
2017-01-01 07:52:02 -0600

    fixes general control

--- resources/views/general/index.blade.php
+++ resources/views/general/index.blade.php
@@ -40,12 +40,16 @@
                                     <td>{{  App\Unity::getNameById($unity_data->unity_id) }}</td>
                                     <td>{{ $unity_data->patient_name }}</td>
                                     <td>{{  App\User::getNameById($unity_data->pilot_id) }}</td>
-                                    <td>{{  App\User::getNameById($unity_data->asistant_id) }}</td>
-                                    <td>{{  App\User::getNameById($unity_data->user_id) }}</td>
-                                    <td>Q {{ $unity_data->patient_input }}/{{ $unity_data->patient_phone }}</td>
-                                    <td>{{ $unity_data->general_case }}</td>
+                                    @if(App\User::getNameById($unity_data->asistant_id) != '')
+                                        <td>{{  App\User::getNameById($unity_data->asistant_id) }}</td>
+                                    @else
+                                        <td>NINGUN ASISTENTE</td>
+                                    @endif
+                                <td>{{  App\User::getNameById($unity_data->user_id) }}</td>
+                                <td>Q {{ $unity_data->patient_input }}/{{ $unity_data->patient_phone }}</td>
+                                <td>{{ $unity_data->patient_case }}</td>
                                 </tr>
-                            @endforeach
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -56,7 +60,7 @@
 @endsection
 @section('after_scripts')
     <script>
-        $(document).ready(function(){
+        $(document).ready(function () {
             $('#unity').DataTable({
                 "language": {
                     "url": "/datatable/language/spanish.json"
