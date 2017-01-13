1c1b97d5a8b4cdd7674a9e635eac4e09f0d6c6bb
Jose Luis Sicajan Coy <sicajancoy@gmail.com>
2017-01-12 23:55:11 -0600

    developer carrousel with fonts

--- resources/views/developer/index.blade.php
+++ resources/views/developer/index.blade.php
@@ -15,61 +15,55 @@
 
             <div class="panel-body text-center">
                 Desarollador: <strong>Jose Luis Sicajan Coy</strong>
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <div id="carousel_developer" class="carousel slide" data-ride="carousel">
                     <!-- Indicators -->
                     <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                        <li data-target="#carousel_developer" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel_developer" data-slide-to="1"></li>
+                        <li data-target="#carousel_developer" data-slide-to="2"></li>
+                        <li data-target="#carousel_developer" data-slide-to="3"></li>
+                        <li data-target="#carousel_developer" data-slide-to="4"></li>
+                        <li data-target="#carousel_developer" data-slide-to="5"></li>
+                        <li data-target="#carousel_developer" data-slide-to="6"></li>
                     </ol>
 
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
                         <div class="item active">
-                            <img src="{{ url('/images/developer/combustible_view.png') }}"
-                                 alt="Ingreso de datos por unidades" width="460" height="345">
+                            <img src="{{ url('/images/developer/combustible_view.png') }}" width="460" height="345">
                         </div>
 
                         <div class="item">
-                            <img src="{{ url('/images/developer/general_view.png') }}"
-                                 alt="Ingreso de datos por unidades" width="460" height="345">
-
+                            <img src="{{ url('/images/developer/general_view.png') }}" width="460" height="345">
                         </div>
 
                         <div class="item">
-                            <img src="{{ url('/images/developer/rd_view.png') }}"
-                                 alt="Ingreso de datos por unidades" width="460" height="345">
-
+                            <img src="{{ url('/images/developer/rd_view.png') }}" width="460" height="345">
                         </div>
 
                         <div class="item">
-                            <img src="{{ url('/images/developer/tdp22_view.png') }}"
-                                 alt="Ingreso de datos por unidades" width="460" height="345">
+                            <img src="{{ url('/images/developer/tdp22_view.png') }}" width="460" height="345">
                         </div>
 
                         <div class="item">
-                            <img src="{{ url('/images/developer/ad21_view.png') }}"
-                                 alt="Ingreso de datos por unidades" width="460" height="345">
+                            <img src="{{ url('/images/developer/ad21_view.png') }}" width="460" height="345">
                         </div>
 
                         <div class="item">
-                            <img src="{{ url('/images/developer/general_view_two.png') }}"
-                                 alt="Ingreso de datos por unidades" width="460" height="345">
+                            <img src="{{ url('/images/developer/general_view_two.png') }}" width="460" height="345">
                         </div>
 
                         <div class="item">
-                            <img src="{{ url('/images/developer/pdf_report.png') }}"
-                                 alt="Ingreso de datos por unidades" width="460" height="345">
+                            <img src="{{ url('/images/developer/pdf_report.png') }}" width="460" height="345">
                         </div>
                     </div>
 
                     <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <a class="left carousel-control" href="#carousel_developer" role="button" data-slide="prev">
                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                         <span class="sr-only">Anterior</span>
                     </a>
-                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <a class="right carousel-control" href="#carousel_developer" role="button" data-slide="next">
                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                         <span class="sr-only">Siguienre</span>
                     </a>
