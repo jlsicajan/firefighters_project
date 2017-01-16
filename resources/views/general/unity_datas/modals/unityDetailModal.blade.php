8339d95e938baa37b89779384f4b85d2683c1391
Jose Luis Sicajan Coy <sicajancoy@gmail.com>
2017-01-15 22:09:51 -0600

    small changes

--- resources/views/general/unity_datas/modals/unityDetailModal.blade.php
+++ resources/views/general/unity_datas/modals/unityDetailModal.blade.php
@@ -11,23 +11,16 @@
         <label for="user_id">Oficial que reporta</label>
         <input size="30" class="form-control" id="user_id" type="text" value="{{  App\User::getNameById($unity_data->user_id) }}" disabled>
     </div>
-</form>
-<form action="#" autocomplete="off" method="POST" id="form_collections">
-    <div class="col-xs-4">
-
-    </div>
-    <div class="col-xs-2">
-
-    </div>
-    <div class="col-xs-3">
-
+    <div class="form-group">
+        <label for="date">Fecha</label>
+        <input size="20" class="form-control" id="date" type="text" value="{{ $unity_data->date }}" disabled>
     </div>
     <div class="form-group">
-        <label for="quantity">Cuanto se recaudo (Q)</label>
-        <input type="number" class="form-control" name="quantity" id="quantity" required/>
+        <label for="unity">Unidad</label>
+        <input size="10" class="form-control" id="unity" type="text" value="{{  App\Unity::getNameById($unity_data->unity_id) }}" disabled>
     </div>
-    <div class="form-group">Breve desripcion</label>
-        <textarea class="form-control" id="station_description" name="station_description"
-                  required rows="8"></textarea>
+    <div class="form-group">
+        <label for="user_id">Oficial que reporta</label>
+        <input size="30" class="form-control" id="user_id" type="text" value="{{  App\User::getNameById($unity_data->user_id) }}" disabled>
     </div>
 </form>
